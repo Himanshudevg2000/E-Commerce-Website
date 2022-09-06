4a85b14eb658396bcccc15b15741aaaea88bcd1d
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-09-06 15:24:30 +0530

    add products to cart

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
+// const cart_items = document.querySelector('#cart .cart-items')
+// const parentContainer = document.getElementById('EcommerceContainer');
+// parentContainer.addEventListener('click',(e)=>{
+//     if(e.target.className == 'shop-item-button'){
+//         const id = e.target.parentNode.parentNode.id
+//         const name = document.querySelector(`#${id} h3`).innerText;
+//         const img_src = document.querySelector(`#${id} img`).src;
+//         const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
+
+//         const cart_item = document.createElement('div');
+//         cart_item.innerHTML = `
+//         <span class='cart-item cart-column'>
+//         <img class='cart-img' src="${img_src}" alt="">
+//             <span>${name}</span>
+//     </span>
+//     <span class='cart-price cart-column'>${price}</span>
+//     <span class='cart-quantity cart-column'>
+//         <input type="text" value="1">
+//         <button>REMOVE</button>
+//     </span>`
+        // cart_items.appendChild(cart_item)
+        //     const container = document.getElementById('container');
+        //     const notification = document.createElement('div');
+        //     notification.classList.add('notification');
+        //     notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
+        //     notification.style = `background-color: steelblue; padding: 20px`
+        //     container.appendChild(notification);
+        //     setTimeout(()=>{
+        //         notification.remove();
+        //     },2500)
+//         }
+
+//         if (e.target.className=='cart-holder'){
+//                     document.querySelector('#cart').style = "display:block;"
+//         }
+
+//         if (e.target.className=='cancel'){
+//                     document.querySelector('#cart').style = "display:none;"
+//         }
+// })
+
+
 const cart_items = document.querySelector('#cart .cart-items')
-const parentContainer = document.getElementById('EcommerceContainer');
-parentContainer.addEventListener('click',(e)=>{
-    if(e.target.className == 'shop-item-button'){
-        const id = e.target.parentNode.parentNode.id
-        const name = document.querySelector(`#${id} h3`).innerText;
-        const img_src = document.querySelector(`#${id} img`).src;
-        const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
+const parentContainer = document.getElementById("EcommerceContainer");
 
-        const cart_item = document.createElement('div');
-        cart_item.innerHTML = `
-        <span class='cart-item cart-column'>
-        <img class='cart-img' src="${img_src}" alt="">
-            <span>${name}</span>
-    </span>
-    <span class='cart-price cart-column'>${price}</span>
-    <span class='cart-quantity cart-column'>
-        <input type="text" value="1">
-        <button>REMOVE</button>
-    </span>`
-        cart_items.appendChild(cart_item)
-            const container = document.getElementById('container');
-            const notification = document.createElement('div');
-            notification.classList.add('notification');
-            notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
-            notification.style = `background-color: steelblue; padding: 20px`
-            container.appendChild(notification);
-            setTimeout(()=>{
-                notification.remove();
-            },2500)
-        }
+parentContainer.addEventListener('click', (e)=> {
 
-        if (e.target.className=='cart-holder'){
-                    document.querySelector('#cart').style = "display:block;"
-        }
+    if (e.target.className=='cart-holder'){
+        document.querySelector('#cart').style = "display:block;"
+   }
+    if (e.target.className=='cancel'){
+        document.querySelector('#cart').style = "display:none;"
+    }
 
-        if (e.target.className=='cancel'){
-                    document.querySelector('#cart').style = "display:none;"
-        }
 })
 
 
@@ -51,8 +66,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 const productHtml = `
                 <div>
                     <h1> ${product.title} </h1>
-                    <img src=${product.imageUrl}> </img>
-                    <button  class="shop-item-button" type='button'>ADD TO CART</button>
+                    <img style="width: 300px" src=${product.imageUrl}> </img>
+                    <button onClick="addToCart(${product.id})"  type='button'>ADD TO CART</button>
                 `
                 parentSection.innerHTML = parentSection.innerHTML + productHtml;
             });
@@ -60,3 +75,31 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 })
+
+function addToCart(productId){
+    axios.post('http://localhost:3000/cart', {productId : productId})
+        .then(response => {
+            // console.log(response)
+            if(response.status === 200){
+                notify(response.data.message)
+            }else{
+                throw new Error()
+            }
+        })
+        .catch((err) => {
+            // console.log(err)
+            notify(err.data.message)
+        })
+}
+
+function notify(message){
+    const container = document.getElementById('container');
+    const notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.innerHTML = `<h4>${message}<h4>`;
+    notification.style = `background-color: steelblue; padding: 20px`
+    container.appendChild(notification);
+    setTimeout(()=>{
+        notification.remove();
+    },2500)
+}
