3efd8e7f4ea2f0ce341bea09fe8a5cf2f78514b8
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-09-12 19:29:59 +0530

    completed

--- script.js
+++ script.js
@@ -20,11 +20,9 @@ parentContainer.addEventListener('click', (e)=> {
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    // e.preventDefault()
             axios.get(`http://localhost:3000/products?page=1`).then((data) => {
                 // console.log(data)
             if(data.request.status === 200){
-                // for(let i=0; i< data.data.length; i++){
                 const products = data.data.products;
                 const parentSection = document.getElementById('Products');
                 
@@ -42,7 +40,6 @@ window.addEventListener('DOMContentLoaded', (e) => {
                     `
                     parentSection.innerHTML = parentSection.innerHTML + productHtml;
                 });
-            // }
             }
         })
 })
@@ -84,7 +81,7 @@ function showCartDetails(){
                 response.data.products.forEach(product => {
                         
                         const cartContainer = document.getElementById('cart-items');
-                        cartContainer.innerHTML += `<li id='cart_li'> <img style="width: 80px" src=${product.imageUrl}> </img> - ${product.price} <button id='Rmvcrt_btn' onclick="cartRemove()" >REMOVE</button>  </li>`
+                        cartContainer.innerHTML += `<li id='cart_li'> <img style="width: 80px" src=${product.imageUrl}> </img> - ${product.price} <button id='Rmvcrt_btn' onclick='cartRemove(${product.id})' >REMOVE</button>  </li>`
                         cartContainer.style = "list-style-type: none; padding: 1rem;"
 
                         let price = document.querySelector('#total-value').innerText;
@@ -104,19 +101,21 @@ function showCartDetails(){
 
 
 function cartRemove(productId) {
-    // axios.delete(`http://localhost:3000/cart`, {productId: productId} )
-    const cartContainer = document.getElementById('cart_li');
-    cartContainer.innerHTML = ' '
-    // .then(response => {
-    //         console.log("deleted from cart")
-    //     })
-    //     .catch(err => {
-    //         console.log("Error Something went Wrong")
-    //     })
-    }
-
-// const cartRemove = document.getElementById('Rmvcrt_btn')
-// const cartContainer = document.getElementById('cart-items');
+    axios.post(`http://localhost:3000/cart-delete-item`, {productId: productId} )
+        .then(response => {
+            if(response.status === 200){
+                let price = document.querySelector('#total-value').innerText;
+                price = parseFloat(price) - parseFloat(document.querySelector('#total-value').innerText)
+                document.querySelector('#total-value').innerText = parseInt(document.querySelector('.cart-number').innerText) - 1
+                document.querySelector('#total-value').innerText = `${price}`
+                productId.target.parentNode.parentNode.remove()
+            }
+        })
+        .catch(err => {
+            console.log("Removed from cart")
+        })
+        console.log('button working')
+}
 
 const pagination_btn = document.querySelector('.pagination');
 pagination_btn.addEventListener('click',(e)=>{
@@ -176,12 +175,17 @@ last_btn.addEventListener('click',(e)=>{
 })
 
 const purchaseBtn = document.getElementById('purchase-btn')
-purchaseBtn.addEventListener('click', (productId) =>{  
+purchaseBtn.addEventListener('click', (productId) =>{
+    if(parseInt(document.getElementById('total-value').innerText) === 0){
+        alert("First add something in your cart! ")
+    }
+    else{
         axios.post('http://localhost:3000/create-order', { productId: productId})
         .then(response => {
             console.log('purchased')
             if(response.status === 200){
-                notify(response.data.message)
+                // notify(response.data.message)
+                alert("Purchased successfully")
             }else{
                 throw new Error(response.data.message)
             }
@@ -189,54 +193,5 @@ purchaseBtn.addEventListener('click', (productId) =>{
         .catch(error => {
             console.log('Error: something went wrong')
         })
-        console.log('NO NO NO')
+    }
 })
-
-
-
-// if (e.target.innerText=='REMOVE'){
-//     let _id = e.target.id;
-//     console.log(_id);
-//     let response = await axios.delete(`http://localhost:4000/cart/${_id}`);
-//     console.log(response);
-//     if(response.data.success==true){
-//         let total_cart_price = document.querySelector('#total-value').innerText;
-//         total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
-//         //needs to remove quantity instead of -1 
-//         cartNumber.innerText = parseInt(cartNumber.innerText)-1
-//         document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
-//         e.target.parentNode.parentNode.remove();
-//     }
-
-
-// if (e.target.className=='purchase-btn'){
-//     if (parseInt(document.querySelector('.cart-number').innerText) === 0){
-//         alert('You have Nothing in Cart');
-//         return
-//     }
-//     let response = await axios.post(`http://localhost:4000/orders/addOrder`);
-//     if(response.data.success == true){
-//         alert('Thanks for the purchase')
-//         cart_items.innerHTML = "";
-//         document.querySelector('.cart-number').innerText = 0;
-//         document.querySelector('#total-value').innerText = `0`;
-//         const container = document.getElementById('container');
-//         const notification = document.createElement('div');
-//         notification.classList.add('notification');
-//         notification.innerHTML = `<h4>SUCCESS ! : Your Order [ OrderId: ${response.data.orderId} ] has been placed successfully!<h4>`;
-//         container.appendChild(notification);
-//         setTimeout(()=>{
-//             notification.remove();
-//         },5000)
-//     }else{
-//         const container = document.getElementById('container');
-//         container.style.backgroundColor = "rebeccapurple";
-//         const notification = document.createElement('div');
-//         notification.classList.add('notification');
-//         notification.innerHTML = `<h4 style="color : Orange;">ERROR ! : Your Order cannot be placed! Please Try Again<h4>`;
-//         container.appendChild(notification);
-//         setTimeout(()=>{
-//             notification.remove();
-//         },5000)
-//     }
-// }
